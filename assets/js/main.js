a79ccd5be6dda3a37efbea60140e5a5a7c02af05
youngleek0918 <youngleek0918@gmail.com>
2021-02-16 03:39:09 +0900

    added projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,3 +75,12 @@ function activePortfolio(){
     }
 }
 linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
+
+
+// GSAP ANIMATION
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, x: 60})
+gsap.from('.home__data', {opacity: 0, duration: 2, delay: .8, y: 25})
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay: 1, y: 25, ease: 'expo.out', stagger: .2})
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay: 1.5, y: 25, ease: 'expo.out', stagger: .2})
+gsap.from('.nav__item', {opacity: 0, duration: 2, delay: 1.8, y: 25, ease: 'expo.out', stagger: .2})
+gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay: 2.3, y: 25, ease: 'expo.out', stagger: .2})
